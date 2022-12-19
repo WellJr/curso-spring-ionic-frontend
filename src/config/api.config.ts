95249ab998332d166303aac662bf275de7badd79
WellJr <wtc-jr@outlook.com>
2022-12-19 19:44:17 -0300

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    // baseUrl: "http://localhost:8080",
+    baseUrl: "https://we-curso-spring-ionic.herokuapp.com/",
     bucketBaseUrl: "https://curso-spring-ionic-welljr.s3.sa-east-1.amazonaws.com"
 }
